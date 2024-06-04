6840baa49a1e48e3143a336582691a722e8aa93c
RAFAEL TORICES <torices.rafael@gmail.coml>
2024-06-04 14:47:56 +0200

    update views

--- app/resources/views/welcome.blade.php
+++ app/resources/views/welcome.blade.php
@@ -866,6 +866,7 @@
                 </div>
 
                 <div class="text-center text-sm text-gray-500 dark:text-gray-400 sm:text-right sm:ml-0">
+                    Welcome<br>
                     Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
                 </div>
             </div>
